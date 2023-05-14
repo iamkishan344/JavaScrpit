71ceed8ac5dbbcb597a548c28b7a85568aa192b3
Kishan <kishanwaghela@05.com>
2023-05-14 21:33:13 +0530

    JavaScrpit Practice

---
+++ every/1.js
@@ -0,0 +1,62 @@
+// comments can make code readable4
+/* Welcome to 30DaysOfJavaScript */
+
+// var my_Name = `JavaScript`;
+// console.log(my_Name);
+
+/* let age = 16;
+if (age <= 18) {
+  console.log("true");
+} else {
+  console.log("false");
+} */
+
+/* let myName;
+console.log(myName); */
+
+/* let mq = null;
+console.log(typeof mq)
+ */
+
+// String Concatenation
+/* let space = ' '
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+let age = 250
+
+fullName =firstName + space + lastName
+let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+
+console.log(personInfoOne) */
+
+// emplate Literals (Template Strings)
+/* let a = 2
+let b = 3
+console.log(`The sum of ${a} and ${b} is ${a + b}`)  */
+
+/* let a = 10;
+function myNeway() {
+  setTimeout(() => {
+    let myWay = "HEY"
+    console.log(myWay);
+  }, 5000);
+}
+myNeway();
+console.log(a); */
+
+/* const food = {
+  Burger: 200,
+  Pizza: 400,
+  Sandwich: 350,
+  juice: 50,
+};
+function getPrice(item) {
+  return food[item];
+}
+console.log(getPrice("Burger")); */
+
+
---
+++ every/array.js
@@ -0,0 +1,101 @@
+// let my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(my_array);
+
+/* for (let i = 0; i < my_array.length; i++) {
+  console.log(my_array[i]);
+} */
+
+/* for (const i in my_array) {
+  console.log([i]);
+} */
+
+/* for (const i of my_array) {
+  console.log(i);
+} */
+
+/* Array */
+/* const vegetables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"];
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length) */
+
+/* const webTechs = ["HTML", "CSS", "JS", "React", "Redux", "Node", "MongDB"];
+console.log("WebTechs:", webTechs);
+console.log("Number of web technologies:", webTechs.length); */
+
+/*  Access Array Element */
+/* const arr = [
+  "Asabeneh",
+  250,
+  true,
+  { country: "Finland", city: "Helsinki" },
+  { skills: ["HTML", "CSS", "JS", "React", "Python"] },
+];
+
+console.log(arr[4].skills[1]);
+console.log(arr);
+*/
+
+/* Creating an array using split */
+
+/* let js = "JavaScript";
+const injs = js.split("");
+console.log(injs); */
+
+/* let companiesString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+const companies = companiesString.split(",");
+console.log(companies); */
+/* 
+let txt =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+const words = txt.split(" ");
+console.log(words); */
+
+/* let num = 6;
+if (num % 2 == 0) {
+  console.log(`${num} is Even`);
+} else {
+  console.log(`${num} is Odd`);
+} */
+/* Print sum of 10  */
+/* let total = 0;
+let num = 10;
+for (let i = 0; i <= num; i++) {
+  total = total + i;
+  console.log(`${total} = ${total} + ${i}`);
+}
+console.log(total); */
+
+/* const number = 10;
+const n = 10;
+for (let i = 0; i <= n; i++) {
+  const result = i * number;
+  console.log(`${number} * ${i} = ${result} `);
+} */
+
+/* function buildNumeber (number){
+  
+} */
+
+
+
+
+let array = [3, 4, 5, 6, 7, 8, 9]
+// let newObject = {}
+for (let index = 0; index < array.length; index++) {
+  // const element = array[index];
+  // var nameHi = "HI";
+  // let newString = `${element}: ${nameHi}`;
+  // newObject = array[i];
+  // var bi= index += 1
+  console.log(array[index]  += 1);
+}
+
+// console.log(bi);
+
+/* {
+  2, "HI", "HI";
+  3, "HI", "HI", "HI";
+} */
+
+
+
